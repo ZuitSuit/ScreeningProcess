a45e503c02e5f180775dafc92b4665d8d6e2501f
ZuitSuit <zootziot@gmail.com>
2021-06-07 10:12:56 +0300

    Camera movement

--- Assets/Scripts/ControllableCamera.cs
+++ Assets/Scripts/ControllableCamera.cs
@@ -7,12 +7,30 @@ public class ControllableCamera : MonoBehaviour
 {
 
     [MinMaxSlider(-180f, 180f)]
-    public Vector2 vertical = new Vector2(-30f, 30f);
+    public Vector2 topDownClamp = new Vector2(0f, 30f);
     [MinMaxSlider(-180f, 180f)]
-    public Vector2 horizontal = new Vector2(-30f, 30f);
+    public Vector2 leftRightClamp = new Vector2(-30f, 30f);
 
-    void Update()
+    Vector3 startingRotation, currentRotation;
+    float horizontalMovement, verticalMovement;
+    public float movementSpeed = 20f;
+    public Transform rotationJoint;
+
+    private void Awake()
     {
-        
+        currentRotation = startingRotation = rotationJoint.transform.localRotation.eulerAngles;
     }
+
+
+    private void Update()
+    {
+        horizontalMovement = Time.deltaTime * (Input.GetKey(KeyCode.LeftArrow) ? -movementSpeed : Input.GetKey(KeyCode.RightArrow) ? movementSpeed : 0f);
+        verticalMovement = Time.deltaTime * (Input.GetKey(KeyCode.UpArrow) ? -movementSpeed : Input.GetKey(KeyCode.DownArrow) ? movementSpeed : 0f);
+
+        currentRotation.y = Mathf.Clamp(currentRotation.y + horizontalMovement, leftRightClamp.x, leftRightClamp.y);
+        currentRotation.x = Mathf.Clamp(currentRotation.x + verticalMovement, topDownClamp.x, topDownClamp.y);
+
+        rotationJoint.transform.rotation = Quaternion.Euler(currentRotation);
+    }
+
 }
